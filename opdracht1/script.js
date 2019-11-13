00b3866f2aefc05bdfcddb2f7a8bef457c41c4d0
BruceAmende <bruceamende32@gmail.com>
2019-11-13 11:44:42 +0100

    opdr1

--- opdracht1/script.js
+++ opdracht1/script.js
@@ -1,25 +1,50 @@
 // A) maak 2 verschillende variabelen met twee willekeurige integers (zelf kiezen)
+var getal1 = 20;
+var getal2 = 45;
 
-// jouw code hierondeer-->> 
-var ;
-var ;
+
 
 // B) toon beide getallen op het scherm met document.getElementById('<jouw_gekozen_id>').innerHTML
+document.getElementById("getal1").innerHTML = getal1;
+document.getElementById("getal2").innerHTML = getal2;
 
-// jouw code hieronder-->>
-document.getElementById
+
 
 // C) Vraag de gebruiker om een INPUT waarde door prompt() te gebruiken. 
 // Gebruik de prompt()-waarde om een variabelen te 'vullen'
+var doosmetgetal1= prompt("geef een getal");
+var doosmetgetal2= prompt("geef een geta2");
+
+document.getElementById("doosmetgetal1").innerHTML = doosmetgetal1;
+document.getElementById("doosmetgetal2").innerHTML = doosmetgetal2;
+
 
 // jouw code hieronder-->>
-var ;
-var ;
+// var ;
+// var ;
 
 // D) Vergelijk de twee getallen met elkaar door gebruik te maken van conditions  if()
 
-// jouw code hieronder-->>
-if () {
+    if( doosmetgetal1 > doosmetgetal2 ){
+        document.getElementById("main").style.backgroundColor = "green";
 
-}
+    }
+
+    
+    if( doosmetgetal1 < doosmetgetal2 ){
+        document.getElementById("main").style.backgroundColor = "red";
+
+    }
+    
+
+
+    
+
+
+
+
+// jouw code hieronder-->>
+// if () {
+
+// }
 
